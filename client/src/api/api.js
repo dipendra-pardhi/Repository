379692349f37a76e98ca8870120ab9021ba7c210
Dipendra Pardhi <dipendrapardhi622@gmail.com>
2026-04-01 13:55:19 +0530

    Update API base URL for the application

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://personal-portfolio-7bxx.onrender.com/api";
+const API_BASE_URL = "https://personal-portfolio-uh6u.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
