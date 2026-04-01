108b5f3f86f67644409144b6495a0eb0d91fe6b9
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2026-04-01 13:45:39 +0530

    Change API base URL to production endpoint

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:5000/api";
+const API_BASE_URL = "https://personal-portfolio-7bxx.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
